9bf248bf48ed639e8e1801ea52680bf6ee04df43
unknown <apridayu@gmail.com>
2013-09-28 16:55:49 +0700

    commit03

---
+++ src/BiodataKu.java
@@ -0,0 +1,12 @@
+//Perintah penggunaan package versi 01
+package Latihan01.sesi1.bin;
+
+public class BiodataKu
+{
+	public static void main(String[] data) {
+		System.out.println("Nama          : Amanda Pridayu");
+		System.out.println("Alamat        : Depok I");
+		System.out.println("Golongan Darah: B");
+		System.out.println("Jenis Kelamin : Perempuan");
+	}
+}
