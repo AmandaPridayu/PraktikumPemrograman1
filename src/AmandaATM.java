0bf640f7fd7efb279eece3b957480ec7e7303cb4
unknown <apridayu@gmail.com>
2013-09-28 17:27:58 +0700

    commit05

---
+++ src/AmandaATM.java
@@ -0,0 +1,13 @@
+//Latihan Membuat Informasi ATM
+package Latihan01.sesi1.bin;
+
+public class AmandaATM
+{
+	public static void main(String[] data) {
+		System.out.println("Nama Nasabah  : Amanda Pridayu");
+		System.out.println("Nama Bank     : Bank Mandiri");
+		System.out.println("No.Kartu ATM  : 4097 6624 2572 2801");
+		System.out.println("Jenis Kartu   : VISA");
+		System.out.println("Berlaku Hingga: 06/22");
+	}
+}
