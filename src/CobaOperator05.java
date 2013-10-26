a8063bd34e455d5a045720d7d00761d3c326cbb3
unknown <apridayu@gmail.com>
2013-10-26 20:26:21 +0700

    commit1

---
+++ src/CobaOperator05.java
@@ -0,0 +1,32 @@
+//Latihan Operator
+
+package Latihan01.sesi1.bin;
+
+public class CobaOperator05
+ {
+   public static void main(String[] Xx) {
+   
+      int c,d,e,f,g;
+	  c=3;
+	  System.out.println("Nilai Awal C : "+c);
+	  c=c+7;
+	  System.out.println("C=C+7 : " +c);
+	  d=5;
+	  System.out.println("Nilai Awal D : "+d);
+	  d=d-4;
+	  System.out.println("D=D-4 : " +d);
+	  e=4;
+	  System.out.println("Nilai Awal E : "+e);
+	  e=e*5;
+	  System.out.println("E=E*5 : " +e);
+	  f=6;
+	  System.out.println("Nilai Awal F : "+f);
+	  f=f/3;
+	  System.out.println("F=F/3 : " +f);
+	  g=12;
+	  System.out.println("Nilai Awal G : "+g);
+	  g=g&7;
+	  System.out.println("G=G%7 : " +g);
+	  
+	}
+}
