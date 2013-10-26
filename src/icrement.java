e9a22abce600cfb9d70595c8e032385e56c96e34
unknown <apridayu@gmail.com>
2013-10-26 19:45:22 +0700

    commit0

---
+++ src/icrement.java
@@ -0,0 +1,37 @@
+/**
+*class increment
+*@author (Amanda)
+*version (op-02)
+*/
+
+package Latihan01.sesi1.bin;
+
+public class icrement
+{
+   public static void main(String[] args)
+  {
+     int a;
+	 a=120;
+	 System.out.println ("Contoh postfix increment : ");
+	 System.out.println (a);
+	 System.out.println (a++);
+	 System.out.println (a);
+	 a=120;
+	 System.out.println ("Contoh prefix increment : ");
+	 System.out.println (a);
+	 System.out.println (++a);
+	 System.out.println (a);
+	 a=120;
+	 System.out.println ("Contoh postfix decrement : ");
+	 System.out.println (a);
+	 System.out.println (a--);
+	 System.out.println (a);
+	 a=120;
+	 System.out.println ("Contoh prefix decrement : ");
+	 System.out.println (a);
+	 System.out.println (--a);
+	 System.out.println (a);
+  }
+}
+	 
+	 
