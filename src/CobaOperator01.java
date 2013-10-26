5ab196685a2b0b5539fc7cef1460468042e5c037
unknown <apridayu@gmail.com>
2013-10-26 19:23:12 +0700

    commit00

---
+++ src/CobaOperator01.java
@@ -0,0 +1,21 @@
+//Perintah Operator
+
+package Latihan01.sesi1.bin;
+
+public class CobaOperator01
+{
+   public static void main(String[] Xx)
+   {
+      int x=7;
+	  System.out.println("Nilai awal x adalah " +x);
+	  System.out.println("Nilai awal x++ adalah " + x++);
+	  System.out.println("Nilai x adalah " +x);
+	  System.out.println();
+	  int y=25;
+	  System.out.println("Nilai awal y adalah " +y);
+	  System.out.println("Nilai ++y adalah " + ++y);
+	  System.out.println("Nilai y adalah " +y);
+	 }
+}
+	  
+	  
