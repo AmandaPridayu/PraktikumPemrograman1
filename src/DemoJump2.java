31d950757f090f4e3f7cfa51b06f112c0fe13087
unknown <apridayu@gmail.com>
2013-11-09 18:01:08 +0700

    commitb

---
+++ src/DemoJump2.java
@@ -0,0 +1,23 @@
+//Perintah Control Flow/Struktur Kontrol
+//Perintah Kelima-Contoh 05
+
+package Latihan01.sesi1.bin;
+
+importjava.util.Random;
+
+public class DemoJump2
+{
+public static void main(String[] Xx)
+{
+   Integerfaktor=7;
+   Integerjumlahfaktordiinginkan=4;
+   IntegerbatasAtas=1000000;
+   
+   System.out.println("Mencari Kelipatan" +faktor+"dengan break dan Continue");
+   Integerjumlahfaktorditemukan=0;
+   while(true)
+   {
+      Integerrandom=newRandom().nextInt();
+	  System.out.Println("Bilangan Random : " +random);
+	  
+   
