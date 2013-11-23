a8a0a5888e2fa3b15e5a4f50b98688bc5c40bb97
unknown <apridayu@gmail.com>
2013-11-23 20:09:42 +0700

    commit12

---
+++ src/Methode1.java
@@ -0,0 +1,31 @@
+package Latihan01.sesi1.bin;
+import java.io.*;
+public class Methode1   {
+   static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+   public static void main(String[] Xx)   {
+   try          {
+      System.out.println("Pilih [1/2/3] : " + "\n\t1. Udang" + "\n\t2. Kepiting" + "\n\t3. Kerang");
+	  System.out.println();
+	  System.out.println("Masukkin Pilihan Anda : ");
+	  String choose = input.readLine();
+	  if(choose.equals("1"))    {
+	     System.out.println("Anda Memilih UDANG");
+	  }
+	  else
+	  if(choose.equals("2"))    {
+	     System.out.println("Anda Memilih KEPITING");
+	  }
+	  else
+	  if(choose.equals("3"))    {
+	     System.out.println("Anda Memilih KERANG");
+      }
+	  else                      {
+	     throw new Exception("Pilihan Anda Tidak Tersedia !!!");
+	  }
+	}
+	catch(Exception e)          {
+	   System.out.println(e.getMessage());
+	   }
+	}
+}
+	  
